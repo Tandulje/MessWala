6f2d48b938b69fc79a046a794bc1befce3558c1d
Nand <nand.tandulje@accops.com>
2020-05-25 00:37:02 +0530

    Deploy in Doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppDemo';
+  title = 'AppDemo for deoplyment on git hub';
 }
